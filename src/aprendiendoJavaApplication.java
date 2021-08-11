ae12bdd7b578b9e90b01b0a7f205f372dd4d9823
Ignacio Santana <ijmsantana@gmail.com>
2021-08-10 22:12:22 -0300

    frist comit

---
+++ src/aprendiendoJavaApplication.java
@@ -0,0 +1,130 @@
+import object.Auto;
+
+public class aprendiendoJavaApplication {
+
+    public static void main(String[] args) {
+
+
+        //OBJETO TIPO STRING DE JAVA lo que quiera puto el que lee
+        //awdawdawdawd
+        /*
+        dwadaw
+        awfawf
+        wfawf
+        awfaw
+        fwafa
+        wf
+        awawgawg
+        awgawg
+        */
+
+        //VARIABLES PRIMITIVAS: boolean, int, string, char, double, float, long.
+        //VARIABLES EN JAVA, LIBRERIAS PROPIAS DE JAVA: Boolean, Integer, String, Double, Float...
+
+        String nombre = "nacho";
+
+        int largo = nombre.length();
+        Integer largo2 = nombre.length();
+        String largoEnLetras = largo2.toString();
+
+        Boolean respuesta = nombre.equals("pepe");
+
+
+//        CONDICIONALES
+//        if(condicion -> true o false, booleana){
+//          Respuesta si es verdadero
+//        } else {
+//          Respuesta si es falso
+//        }
+
+        if(nombre.equals("nacho")){
+            System.out.println("Es nacho");
+        } else {
+            System.out.println("No Es nacho");
+        }
+
+        //EN NUMEROS SE COMPARA CON ==, >=, <=, >, <, !=
+        if(largo > 8){
+            System.out.println("Largo es mayor a 8");
+        } else if (largo > 2){
+            //EJECUCION
+        } else {
+            //EJECUCION
+        }
+
+        //CONDICIONAL MULTIPLE
+        switch (nombre){
+            case "nacho":
+                System.out.println("Es nacho");
+                break; //SE PONE BREAK PARA SALIR DEL SWITCH
+            case "pepe":
+                System.out.println("Es pepe");
+                break;
+            default:
+                System.out.println("es otro nombre");
+        }
+
+        //CONDICIONAL TERNARIO -> IF REDUCIDO
+        //CONDICION ? RESULTADO VERDADERO : RESULTADO FALSO;
+        String resultado2 = nombre.equals("nacho") ? "NACHO" : "NO ES NACHO";
+
+
+        //CICLOS EXACTOS -> FOR
+        //FOR(DESDE DONDE ARRANCO; CONDICION Hasta donde; SALTO)
+        // i++ -> Va acumulando 1
+        for(int i=0; i<10;i++){
+
+            if(i==2){
+                continue; //SALE DE ESA ITERACION Y VA A LA SIGUIENTE
+            }
+
+            System.out.println(i); // 0, 1, 3, 4,5,6,7,8
+
+            if (i == 8) {
+                break; //SALGA AUTOMATICAMENTE DEL FOR.
+            }
+
+        }
+
+        //CICLOS INEXACTOS -> SOLO ENTRA A ITERAR SI CUMPLE UNA CONDICION
+        while(largo<7){
+            System.out.println(largo);
+            largo++;
+        }
+
+
+
+        //OBJETOS:
+
+        //OBJETO TIPO AUTO QUE DEFINIMOS NOSTROS
+        Auto corsa = new Auto();//SE TIENEN QUE INICIALIZAR LAS VARIABLES.
+        corsa.mostrarPatente();
+
+
+
+
+        Auto corsa2 = new Auto();
+
+        //COPIANDO AUTO2 DE CORSA EN UN OBJETO NUEVO
+        Auto auto2 = new Auto();
+        auto2.copiarAuto(corsa);
+
+        corsa.setearPatente("HAD123");
+
+        corsa2.setearPatente(corsa.mostrarPatente());
+
+
+        System.out.println(auto2.mostrarPatente());
+        System.out.println(corsa2.mostrarPatente());
+        System.out.println(corsa.mostrarPatente());
+
+        //String patente = corsa.mostrarPatente();
+
+        //System.out.println(corsa);
+
+    }
+
+
+
+
+}
---
+++ src/object/Auto.java
@@ -0,0 +1,33 @@
+package object;
+
+import java.time.LocalDate;
+
+public class Auto {
+
+    private String patente;
+    private String modelo;
+    private Double peso;
+    private String nroMotor;
+    private LocalDate fechaFabricacion;
+
+/*    FUNCION: Y = F(X1,X2,X3...)
+     QueDevuelve(TIPO DE VARIABLE DE Y) NombreDeFUncion(ENTRADA, ENTRADA2){
+        ALGORITMOS = X2 +3;
+    }*/
+
+    public String mostrarPatente(){
+        return this.patente;
+    }
+
+    //NO RETORNO NADA -> VOID
+    public void setearPatente(String pat){
+        this.patente = pat;
+    }
+
+    public Auto copiarAuto(Auto auto1){
+        Auto auto2 = new Auto();
+        auto2.setearPatente(auto1.mostrarPatente());
+        return auto2;
+    }
+
+}
