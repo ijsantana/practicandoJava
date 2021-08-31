400ed6121a24b4bcfaa3225b2c1a0e2abca200c2
unknown <you@example.com>
2021-08-31 19:45:13 -0300

    actualizo proyecto

--- src/veterinaria/practicandoVeterinaria.java
+++ src/veterinaria/practicandoVeterinaria.java
@@ -1,10 +1,22 @@
 package veterinaria;
 
+import java.util.Scanner;
+
 public class practicandoVeterinaria {
 
     public static void main(String [] args){
+
+        Scanner nombreMascota = new Scanner(System.in);
+             System.out.println("Como se llama tu mascota");
+
+        String nombreM = nombreMascota.nextLine();  // Read user input
+
+
+
+
         veterinaria gato = new veterinaria("Sol",8,"Ian");
         veterinaria perro = new veterinaria("Laica",6,"=Cristina");
+
 
         gato.mostrarDatos();
         gato.verificarEdad();
