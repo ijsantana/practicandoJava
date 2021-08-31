3d9dce22d2c2d750944113cace62970987fb4fd8
unknown <you@example.com>
2021-08-31 19:27:46 -0300

    actualizo proyecto

--- src/veterinaria/practicandoVeterinaria.java
+++ src/veterinaria/practicandoVeterinaria.java
@@ -3,9 +3,13 @@ package veterinaria;
 public class practicandoVeterinaria {
 
     public static void main(String [] args){
-        veterinaria gato = new veterinaria("sol",8,"ian");
+        veterinaria gato = new veterinaria("Sol",8,"Ian");
+        veterinaria perro = new veterinaria("Laica",6,"=Cristina");
 
         gato.mostrarDatos();
+        gato.verificarEdad();
+        perro.mostrarDatos();
+        perro.verificarEdad();
 
     }
 
--- src/veterinaria/veterinaria.java
+++ src/veterinaria/veterinaria.java
@@ -16,10 +16,20 @@ public class veterinaria {
         this.dueno = nombreD;
     }
 
+    //METODOS
+
     public void mostrarDatos() {
         System.out.println("El nombre es:" +mascota);
         System.out.println("La edad es:" +edad);
         System.out.println("El dueno se llama:" +dueno);
+    }
+
+    public void verificarEdad() {
+        if (edad > 10) {
+            System.out.println(mascota + " es mayor de edad");
+        } else {
+            System.out.println(mascota+" es menor de edad");
+        }
     }
 
 }
